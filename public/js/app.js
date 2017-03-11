86611040b832606b242d091713463997c1688527
Pavan Kale <pavankale1234@gmail.com>
2017-03-11 09:11:39 +0530

    Add font-end form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,19 @@ socket.on('message', function (message) {
 	console.log('New message:');
 	console.log(message.text);
 });
+
+// Handles submitting of new message
+
+//$form stores jquery instance of an element with id=message-form
+//That means this variable has access to all the methods that you could access on any jquery element
+var $form = jQuery('#message-form'); 
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		text: $message.val()
+	});
+	$message.val('');
+});
